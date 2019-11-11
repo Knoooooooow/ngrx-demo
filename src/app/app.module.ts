2345e023e7f986713decc2bc6b65637723ada33a
knoooooooow <knoooooooow@outlook.com>
2019-11-11 22:30:39 +0800

    初始化项目

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './core/app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CreateComponent } from './pages/create/create.component';
+import { CompleteComponent } from './pages/complete/complete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CreateComponent,
+    CompleteComponent
   ],
   imports: [
     BrowserModule,
